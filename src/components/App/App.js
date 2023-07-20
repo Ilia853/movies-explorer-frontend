b5210d6b930ced47bbe86c6de7c8517b188abbd9
Ilia Egorov <i.egorov119@yandex.ru>
2023-07-21 00:34:53 +0400

    Register-full

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,26 @@
 // import React, { useEffect, useState } from "react";
 import './App.css';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
+// import Header from '../Header/Header';
+// import Footer from '../Footer/Footer';
 import Main from '../Main/Main';
+import Register from '../Register/Register';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+  // const [loggedIn, setLoggedIn] = useState(false);
+  
   return (
     <div>
-      <Header />
-      <Main />
-      <Footer />
+      <Routes>
+        <Route 
+          path='/'
+          element={<Main />}
+        />
+        <Route 
+          path='/sign-up'
+          element={<Register/>}
+        />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Register/Register.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Register() {
+  return (
+    <section className="register">
+      <div className="register__header-logo-wrapper">
+        <div className="header__logo header__logo_type_register"></div>
+        <h2 className="register__title">Добро пожаловать!</h2>
+      </div>
+      <form className="register__form">
+        <p className="register__field-name">Имя</p>
+        <input className="register__input register__input_type_name" />
+        <span className="register__error"></span>
+        <p className="register__field-name">E-mail</p>
+        <input className="register__input register__input_type_email" />
+        <span className="register__error"></span>
+        <p className="register__field-name">Пароль</p>
+        <input
+          className="register__input register__input_type_password"
+          type="password"
+        />
+        <span className="register__error">Что-то пошло не так...</span>
+        <button className="register__form-button">Зарегистрироваться</button>
+      </form>
+      <div className="register__sign">
+        <p className="register__paragraph">Уже зарегистрированы?</p>
+        <Link to="/sign-in" className="register__login-link">
+          Войти
+        </Link>
+      </div>
+    </section>
+  );
+}
